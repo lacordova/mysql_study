02e7882fe6657107c087441db724e457ea512525
lacordova <lacordova@uc.cl>
2021-03-26 18:41:25 -0300

    first commit

---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'localhost',
+        user: 'root',
+        password: '3341',
+        database: 'database_links'
+    }
+}
---
+++ src/routes/links.js
@@ -0,0 +1,5 @@
+const router = require('express').Router()
+
+const pool = require('../database')
+
+module.exports = router
