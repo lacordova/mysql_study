2de9949ff784663d85ef5663ab655fab10a4dc7d
lacordova <lacordova@uc.cl>
2021-03-27 12:51:54 -0300

    comunicate with MySql CRUD

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,6 +4,7 @@ const pool = require('../database')
 router.get('/add', (req, res) => {
     res.render('links/add')
 })
+
 router.post('/add', async (req, res) => {
     const { title, url, description } = req.body
     const newLink = {
@@ -12,7 +13,38 @@ router.post('/add', async (req, res) => {
         description
     }
     await pool.query('INSERT INTO links set ?', [newLink])
-    res.send('received')
+    res.redirect('/links')
+})
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params
+    await pool.query('DELETE FROM links WHERE ID = ?', [id])
+    res.redirect('/links')
+})
+
+router.get('/', async (req, res) => {
+    const links = await pool.query('SELECT * FROM links')
+    console.log(links)
+    res.render('links/list', {links})
+})
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params
+    const links = await pool.query('SELECT * FROM  links WHERE id = ?', [id])
+    res.render('links/edit', {link: links[0]})
+})
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params
+    const { title, description, url } = req.body
+    const newLink = {
+        title,
+        description,
+        url
+    }
+    console.log(newLink)
+    await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id])
+    res.redirect('/links')
 })
 
 module.exports = router
